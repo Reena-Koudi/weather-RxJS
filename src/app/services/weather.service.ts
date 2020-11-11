884733a1c3b02f87a8dd961685b99e89ace23426
Reena Koudi <reena.koudi@rank.com>
2020-11-11 13:44:00 +0000

    adding getWeather() function in service

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable} from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { Weather } from '../models/weather';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getWeather(city: string): Observable<Weather[]>{
+    return this.http.get<Weather[]>(`${environment.apiEndPoint}/?i=${city}`).pipe(map((items: any) => {
+      return items.results;
+    }),
+    catchError((err: Error) => {
+      return err.message;
+    })
+    );
+  }
 }
