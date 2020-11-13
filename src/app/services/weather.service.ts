e17a9f494c406f25f85387605a4c3e8a134ecb72
Reena Koudi <reena.koudi@rank.com>
2020-11-13 13:25:57 +0000

    adding getWeatherDayForCity(cityId: string) to  weather service

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -5,6 +5,7 @@ import { catchError, map } from 'rxjs/operators';
 import { Weather } from '../models/weather';
 import { environment } from 'src/environments/environment';
 import { title } from 'process';
+import { WeatherDay } from '../models/forecast';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +16,17 @@ export class WeatherService {
 
   getWeatherForCity(city: string): Observable<Weather[]>{
     return this.http.get<Weather[]>(`${environment.apiEndPoint}/location/search/?query=${city}`).pipe(
+      map((items: any) => {
+        return items;
+      }),
+      catchError((err: Error) => {
+        return err.message;
+      })
+    );
+  }
+
+  getWeatherDayForCity(cityId: string): Observable<WeatherDay[]>{
+    return this.http.get<WeatherDay[]>(`${environment.apiEndPoint}/location/${cityId}`).pipe(
       map((items: any) => {
         return items;
       }),
